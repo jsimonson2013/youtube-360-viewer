59ee6839cba549e164172e4677de25f99f7d1479
Jacob Simonson <jsimonson2013@gmail.com>
2018-02-28 16:29:25 -0700

    Executing js from extension

---
+++ scripts/popup.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+	document.getElementById('activate').addEventListener('click', () => {
+		alert('Activated!')
+	})
+	document.getElementById('deactivate').addEventListener('click', () => {
+		alert('Deactivated!')
+	})
+})
